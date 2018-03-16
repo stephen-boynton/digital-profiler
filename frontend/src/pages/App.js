85e9a7852c87616195cdd8d66c720381926d50b1
Stephen Boynton <boyntonsc@gmail.com>
2018-03-16 18:47:14 -0400

    Set up beginnings of frontend

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Homepage from "./Homepage";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </div>
+    );
   }
 }
 
---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomePage(props) {
+  return (
+    <div className="HomePage">
+      <form>Hello world!</form>
+    </div>
+  );
+}
